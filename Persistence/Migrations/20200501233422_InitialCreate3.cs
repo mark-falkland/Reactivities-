9ba644d0a7bca68d4f6ba9e54dbd2b8d7a498061
Mark <mark@Marks-MacBook-Air.local>
2020-05-02 11:07:36 +1000

    Initial Commit

---
+++ Persistence/Migrations/20200501233422_InitialCreate3.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class InitialCreate3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Value a");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Value b");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Value c");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Value 1");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Value 2");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Value 3");
+        }
+    }
+}
